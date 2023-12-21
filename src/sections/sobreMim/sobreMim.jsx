de15e82024f9075e47d16b3fa789701e2a497529
Samuel <samuelsantanadasilva8@gmail.com>
2023-12-21 18:02:45 -0300

    Ajustando o nome

--- src/sections/sobreMim/sobreMim.jsx
+++ src/sections/sobreMim/sobreMim.jsx
@@ -13,7 +13,7 @@ export function SobreMim(){
                 </div>
 
                 <div className='divTextoSobreMim'>
-                    <p>Rogério Gurgel, psicólogo formado pela Universidade Uninassau, especializado em Terapia Cognitivo Comportamental, tem seis anos de experiência atendendo adultos e casais. Suas especialidades incluem Gestão de Pessoas e Terapia Familiar. Ele oferece terapia presencial e online em Águas Claras, com horários flexíveis. Aceita várias formas de pagamento e mantém presença ativa nas redes sociais para compartilhar conteúdo relevante sobre saúde mental e bem-estar.</p>
+                    <p>Rogério Gourgel, psicólogo formado pela Universidade Uninassau, especializado em Terapia Cognitivo Comportamental, tem seis anos de experiência atendendo adultos e casais. Suas especialidades incluem Gestão de Pessoas e Terapia Familiar. Ele oferece terapia presencial e online em Águas Claras, com horários flexíveis. Aceita várias formas de pagamento e mantém presença ativa nas redes sociais para compartilhar conteúdo relevante sobre saúde mental e bem-estar.</p>
                 </div>
             </div>
 
